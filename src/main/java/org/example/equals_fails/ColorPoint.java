ec1b50c1c22505de99c2855d5db5fed80bb40cf0
rollo <rollovitch.roll@yandex.ru>
2023-08-15 23:58:19 +0300

    equals method

--- src/main/java/org/example/equals_fails/ColorPoint.java
+++ src/main/java/org/example/equals_fails/ColorPoint.java
@@ -19,9 +19,14 @@ public class ColorPoint extends Point{
     @Override
     public boolean equals(Object obj){
         //нарушается транзитивность! мы можем вызывать метод от Point и им сравнивать ColorPoint
-        //а при вызове от ColorPoint мы получим ошибку преобразования класса
         // но если мы не будем наследовать и создадим новый метод только для ColorPoint. то
         //нарушим подстановки Лисков
+        if(obj == this)
+            return true;
+
+        if(!(obj instanceof ColorPoint))
+            return false;
+
         return super.equals(obj) &&
                 color.equals(((ColorPoint) obj).color);
     }
