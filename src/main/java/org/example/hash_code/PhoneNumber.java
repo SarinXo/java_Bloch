81c0c0499c83afafde83f9f79f668bf91632b562
rollo <rollovitch.roll@yandex.ru>
2023-08-18 23:59:17 +0300

    hashing

--- src/main/java/org/example/hash_code/PhoneNumber.java
+++ src/main/java/org/example/hash_code/PhoneNumber.java
@@ -4,13 +4,18 @@ public class PhoneNumber {
     private Short prefix;
     private Short areaCode;
     private Short lineNum;
-
+    //Отложенная инициализация
+    private int hashCode;
     //com.google.common.hash.Hashing меньше всего коллизий
     @Override
     public int hashCode() {
-        int result = Short.hashCode(areaCode);
-        result = 31 * result + Short.hashCode(prefix);
-        result = 31 * result + Short.hashCode(lineNum);
-        return result;
+        int result = hashCode;
+        if(hashCode == 0) {
+            result = Short.hashCode(areaCode);
+            result = 31 * result + Short.hashCode(prefix);
+            result = 31 * result + Short.hashCode(lineNum);
+            return result;
+        }
+        return hashCode;
     }
 }
