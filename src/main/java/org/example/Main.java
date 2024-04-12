6ddeb056cadf1da580c2b9a4ed638303246c6e83
rollo <rollovitch.roll@yandex.ru>
2024-04-12 20:44:57 +0300

    :)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
     //usless coment :)
     //another useless comment
     public static void main(String[] args) {
-
+        System.out.println("Hello new merge...");
     }
 }
 
